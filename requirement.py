8800ae02716c49ccc4750187f4233da7f1f14b0e
Loanttp <169128090+Loanttp@users.noreply.github.com>
2024-11-16 08:58:29 +0700

    Update requirement.py

--- requirement.py
+++ requirement.py
@@ -6,3 +6,38 @@ pip install -r requirements.txt
 pip install matplotlib
 pip uninstall streamlit matplotlib
 pip install streamlit matplotlib
+
+
+
+import pandas as pd
+import numpy as np
+import warnings
+import os
+import joblib
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import train_test_split, cross_val_predict, KFold
+from sklearn.metrics import classification_report
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import skew
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier, RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score, KFold
+import numpy as np
+import pandas as pd
+from joblib import load
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
